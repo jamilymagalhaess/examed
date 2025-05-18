73f9b7fad0964620e2092eb6a109bb64ae839607
jamilymagalhaess <magalhaes.jamily@hotmail.com>
2025-05-18 17:23:59 -0400

    feat: agendamento de exames.

--- src/app/pages/agendar-exame/agendar-exame.component.ts
+++ src/app/pages/agendar-exame/agendar-exame.component.ts
@@ -1,11 +1,92 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  DateAdapter,
+  MAT_DATE_FORMATS,
+  MAT_DATE_LOCALE,
+} from '@angular/material/core';
+import {
+  MAT_MOMENT_DATE_ADAPTER_OPTIONS,
+  MomentDateAdapter,
+} from '@angular/material-moment-adapter';
+//import { ExameService } from '../services/exame.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'DD/MM/YYYY',
+  },
+  display: {
+    dateInput: 'DD/MM/YYYY',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 
 @Component({
   selector: 'app-agendar-exame',
-  imports: [],
   templateUrl: './agendar-exame.component.html',
-  styleUrl: './agendar-exame.component.scss'
+  styleUrls: ['./agendar-exame.component.scss'],
+  standalone: false,
+  providers: [
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS],
+    },
+    { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS },
+  ],
 })
-export class AgendarExameComponent {
+export class AgendarExameComponent implements OnInit {
+  agendamentoForm: FormGroup;
+  tiposExame: string[] = [
+    'Hemograma Completo',
+    'Glicemia em Jejum',
+    'Colesterol Total',
+    'Urina Tipo 1',
+    'Eletrocardiograma',
+    'Ultrassonografia Abdominal',
+  ];
 
+  constructor(
+    private fb: FormBuilder,
+    // private exameService: ExameService,
+    private snackBar: MatSnackBar
+  ) {
+    this.agendamentoForm = this.fb.group({
+      nome: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      tipoExame: ['', Validators.required],
+      instrucoes: [''],
+      cpf: ['', Validators.required],
+      cartaoSus: ['', Validators.required],
+      dataExame: ['', Validators.required],
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    if (this.agendamentoForm.valid) {
+      const formData = this.agendamentoForm.value;
+
+      // this.exameService.agendarExame(formData).subscribe({
+      //   next: () => {
+      //     this.snackBar.open('Exame agendado com sucesso!', 'Fechar', {
+      //       duration: 5000,
+      //       panelClass: ['success-snackbar']
+      //     });
+      //     this.agendamentoForm.reset();
+      //   },
+      //   error: (err) => {
+      //     console.error('Erro ao agendar exame:', err);
+      //     this.snackBar.open('Erro ao agendar exame. Tente novamente.', 'Fechar', {
+      //       duration: 5000,
+      //       panelClass: ['error-snackbar']
+      //     });
+      //   }
+      // });
+    }
+  }
 }
--- src/app/pages/agendar-exame/agendar-exame.module.ts
+++ src/app/pages/agendar-exame/agendar-exame.module.ts
@@ -1,14 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { provideNgxMask, NgxMaskDirective, NgxMaskPipe } from 'ngx-mask';
+import { AgendarExameComponent } from './agendar-exame.component';
 import { AgendarExameRoutingModule } from './agendar-exame-routing.module';
 
-
 @NgModule({
-  declarations: [],
   imports: [
+    AgendarExameRoutingModule,
     CommonModule,
-    AgendarExameRoutingModule
-  ]
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    NgxMaskDirective,
+    NgxMaskPipe,
+  ],
+  providers: [provideNgxMask()],
+  declarations: [AgendarExameComponent],
 })
-export class AgendarExameModule { }
+export class AgendarExameModule {}
